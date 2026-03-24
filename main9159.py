b229d919f567349393fc3d54fba70ad46192aec4
myeonghwi <automaster5013@gmail.com>
2026-03-24 15:01:28 +0900

    Python 출력 예제 연습하기

---
+++ main9159.py
@@ -0,0 +1,8 @@
+print('Apple Banana Carrot')
+
+print('Apple', 'Banana', 'Carrot') #구분자(,) 사용
+
+print('Apple','Banana','Carrot', sep = ',')
+
+print('myeonghwi', end = ' ')
+print('Lee')
--- practice2.py
+++ practice2.py
@@ -28,3 +28,21 @@ print('-' * 10)
 print('NadoCording')
 print('*' * 20)
 
+letter = 'how are YOU?'
+print(letter.lower())   #모두 소문자
+print(letter.upper())   #모두 대문자
+print(letter.capitalize())  #전체 문장에서 첫글자만 대문자
+print(letter.title())       #단어의 첫글자만 대문자
+print(letter.swapcase())    #대소문자를 뒤바꾸기(반전)
+print(letter.split())       #문자열 나누기
+
+str1 = letter.split()       #문자열을 나누고 첫글자만 대문자로 만들기
+print(str1[0].capitalize())
+print(str1[1].capitalize())
+print(str1[2].capitalize())
+
+letter = 'how are YOU?'
+print(letter.count('how'))  #특정 단어의 수
+
+letter = 'how are you?'
+print(letter.count('o'))    #특정 글자의 수
