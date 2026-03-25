ce737a99d937cd79c3c591a3537fc0b3ea143995
myeonghwi <automaster5013@gmail.com>
2026-03-25 18:03:00 +0900

    정올 python 예제 코드 연습

--- jungol9203.py
+++ jungol9203.py
@@ -4,6 +4,7 @@ s = a + b + c
 print(f"sum = {s}")
 print(f"avg = {s // 3}")
 
+#####################################################################
 
 # a, b, c = map(int, input().split())
 # # print(a, b, c)
@@ -13,3 +14,27 @@ print(f"avg = {s // 3}")
 # sum = a + b + c
 # print('sum =', sum)
 # print('avg =', sum//3)
+
+#####################################################################
+
+# a, b, c = map(int, input().split())
+# print("sum =", a+b+c)
+# print("avg =", (a+b+c)//3)
+
+#####################################################################
+
+# list1 = list(map(int, input().split()))
+# sum1 = sum(list1)
+# len1 = int(len(list1))
+# ever = int(sum1/len1)
+# print(f"sum = {sum1}")
+# print(f"avg = {ever}")
+
+#####################################################################
+
+# a, b, c = map(int, input().split())
+# sum = a+b+c
+# avg = int((a+b+c)/3)
+
+# print('sum =', sum)
+# print('avg =', avg)
