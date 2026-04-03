2d937f06aaf1308d48dbf4cba6610724db0f256c
myeonghwi <automaster5013@gmail.com>
2026-04-03 18:34:25 +0900

    LC_python_프로그래밍기본_단계별_문제풀이

--- jungol/jungol9350.py
+++ jungol/jungol9350.py
@@ -16,3 +16,31 @@ for i in nums:
 
 ##############################################(방법02)
 
+lst = []
+
+for i in range(5):
+    x= int(input())
+    lst.append(x)
+
+for j in range(len(lst)):
+    print(lst[j], end=' ')
+
+##############################################(방법03)
+
+class S:
+    def __init__(self, N):
+        self.N = N
+    def I(self):
+        if self.N >= 0:
+            Q.append(str(self.N))
+
+Q=[]
+for i in range(5):
+    N = int(input())
+    P = S(N)
+    P.I()
+
+print(f"{' '.join(Q)}")
+
+##############################################(방법04)
+
