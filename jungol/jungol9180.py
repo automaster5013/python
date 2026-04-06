c60d3e21f4d0d973e6b8fc6db6e0330c346265ce
myeonghwi <automaster5013@gmail.com>
2026-04-06 14:12:22 +0900

    LC_python_프로그래밍기본_단계별_문제풀이

---
+++ jungol/jungol9180.py
@@ -0,0 +1,19 @@
+age = int(input())
+# print(age)
+print(f"I am {age} years old")
+
+###########################################(방법01)
+
+age = input()
+# print(age)
+print(f"I am {age} year old")
+
+###########################################(방법02)
+
+N = int(input())
+def Age(N): # 정의부
+    print(f"I am {N} years old")
+Age(N)  # 호출부
+
+###########################################(방법03)
+
